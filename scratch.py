767c6d2b8a5b9870e867e4313e6b8dc83841fc00
prostotyomka <166050739+prostotyomka@users.noreply.github.com>
2026-06-10 14:23:27 +0300

    ver 0.1

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 browser = webdriver.Chrome()
 url = 'http://127.0.0.1:8080/'
@@ -10,13 +10,17 @@ browser.find_element(By.ID,"create-integer").click()
 browser.find_element(By.NAME,'quota_name').send_keys('Artom')
 browser.find_element(By.NAME,'object_value').send_keys(10)
 browser.find_element(By.ID,'i-save').click()
-time.sleep(2)
-
 
 x = browser.find_elements(By.CLASS_NAME,'mb-4.object-item')
 x[0].click()
 browser.find_element(By.NAME,'object_value').clear()
 browser.find_element(By.NAME,'object_value').send_keys(12)
 browser.find_element(By.ID,'i-save').click()
-time.sleep(2)
+
+len_1 = browser.find_elements(By.CLASS_NAME,'mb-4.object-item')
+button = browser.find_element(By.XPATH,'/html/body/div/div[2]/table/tbody/tr/td[4]/button[2]')
+actioncains = ActionChains(browser)
+actioncains.move_to_element(len_1[0]).move_to_element(button).click().perform()
+alert = browser.switch_to.alert
+alert.accept()
 
